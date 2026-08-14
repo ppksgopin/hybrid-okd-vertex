4b5ddc4a6be1828b0c32f974f68de6e43263993e
ppksgopin <ppksgopin@gmail.com>
2026-08-14 19:43:19 +0800

    clear non-using docker image

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 
 docker build -t ansible-infra-img:latest . --no-cache
 docker run -it -d --name ansible-infra --network host -v $(pwd):/ansible -v ~/.ssh:/root/.ssh:ro ansible-infra-img:latest
+docker image prune -af
